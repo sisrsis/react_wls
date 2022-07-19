163171fbc2a21d1a58e3534e159327e56098e7a4
sisrsis <virussisrsis@gmail.com>
2022-07-19 12:50:33 +0430

    create project

---
+++ index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+function Sisrsis() {
+    let sis = 36;
+    console.log(sis);
+
+    return (
+<body>
+<div id="dev_left">
+    <button class="button_felid_left" style={{height:sis}}> 19 </button>
+    <button class="button_felid_left" style={{height:sis}}> 18 </button>
+    <button class="button_felid_left" style={{height:sis}}> 17 </button>
+    <button class="button_felid_left" style={{height:sis}}> 16 </button>
+    <button class="button_felid_left" style={{height:sis}}> 15 </button>
+    <button class="button_felid_left" style={{height:sis}}> 14 </button>
+    <button class="button_felid_left" style={{height:sis}}> 13 </button>
+    <button class="button_felid_left" style={{height:sis}}> 12 </button>
+    <button class="button_felid_left" style={{height:sis}}> 11 </button>
+    <button class="button_felid_left" style={{height:sis}}> 10 </button>
+    <button class="button_felid_left" style={{height:sis}}> 9 </button>
+    <button class="button_felid_left" style={{height:sis}}> 8 </button>
+    <button class="button_felid_left" style={{height:sis}}> 7 </button>
+    <button class="button_felid_left" style={{height:sis}}> 6 </button>
+    <button class="button_felid_left" style={{height:sis}}> 5 </button>
+    <button class="button_felid_left" style={{height:sis}}> 4 </button>
+    <button class="button_felid_left" style={{height:sis}}> 3 </button>
+    <button class="button_felid_left" style={{height:sis}}> 2 </button>
+    <button class="button_felid_left" style={{height:sis}}> 1 </button>          
+		
+	</div>
+    <div id='dev_right'>
+
+    <button class="button_felid_right"style={{height:sis}}> 19 </button>
+    <button class="button_felid_right"style={{height:sis}}> 18 </button>
+    <button class="button_felid_right"style={{height:sis}}> 17 </button>
+    <button class="button_felid_right"style={{height:sis}}> 16 </button>
+    <button class="button_felid_right"style={{height:sis}}> 15 </button>
+    <button class="button_felid_right"style={{height:sis}}> 14 </button>
+    <button class="button_felid_right"style={{height:sis}}> 13 </button>
+    <button class="button_felid_right"style={{height:sis}}> 12 </button>
+    <button class="button_felid_right"style={{height:sis}}> 11 </button>
+    <button class="button_felid_right"style={{height:sis}}> 10 </button>
+    <button class="button_felid_right"style={{height:sis}}> 9 </button>
+    <button class="button_felid_right"style={{height:sis}}> 8 </button>
+    <button class="button_felid_right"style={{height:sis}}> 7 </button>
+    <button class="button_felid_right"style={{height:sis}}> 6 </button>
+    <button class="button_felid_right"style={{height:sis}}> 5 </button>
+    <button class="button_felid_right"style={{height:sis}}> 4 </button>
+    <button class="button_felid_right"style={{height:sis}}> 3 </button>
+    <button class="button_felid_right"style={{height:sis}}> 2 </button>
+    <button class="button_felid_right"style={{height:sis}}> 1 </button>
+    </div>
+
+    </body>
+  
+    );
+
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(< Sisrsis />);
